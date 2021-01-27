24c46e17bcfec9631d4897b0d3af76648459a0dc
Sahira Bibi <70290221+sahirabibi@users.noreply.github.com>
2021-01-27 18:51:34 -0500

    Added hourly and day conditions to main.py
    Hourly forecast text messages will only be sent at 5 am every day forecasting hourly weather from 5 am -8 pm. Weekly forecast will only be sent at the start of the week on Mondays.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ auth_token = os.environ.get("AUTH_TOKEN")
 my_phone_number = "YOUR_PHONE_NUMBER"
 
 class Weather:
-    """Get Weather forcast"""
+    """Get Weather forecast"""
     
     def __init__(self):
 
@@ -28,7 +28,7 @@ class Weather:
         self.current_temp = round(self.current_data["temp"])
         self.weather_id = self.current_data["weather"][0]["id"]
 
-    def weekly_forcast(self):
+    def weekly_forecast(self):
         """
         Return formated forcast for each day of the week, providing min/max
         temp and weather conditions 
@@ -40,51 +40,56 @@ class Weather:
             timestamp = day["dt"]
             weekday = datetime.utcfromtimestamp(timestamp).strftime('%A')
             week_dict[weekday] = day
-            self.week_forcast = ""
+            self.week_forecast = ""
         for day in week_dict:
             # extract and format min/max temp and weather conditions for 7 day
             min_temp = week_dict[day]["temp"]["min"]
             max_temp = week_dict[day]["temp"]["max"]
             weather_conditions = week_dict[day]["weather"][0]["description"]
-            daily_forcast_text = f"Day: {day}\n\tMIN: {min_temp}, MAX: {max_temp},\n\tforcast: {weather_conditions}\n"
-            self.week_forcast += daily_forcast_text
+            daily_forecast_text = f"Day: {day}\n\tMIN: {min_temp}, MAX: {max_temp},\n\tforecast: {weather_conditions}\n"
+            self.week_forecast += daily_forecast_text
         return self.week_forcast
 
-    def hourly_forcast(self):
+    def hourly_forecast(self):
         """Get the weather conditions for the next day from 5am-8pm """
         self.hourly_data = self.data["hourly"][6:22]
-        self.forcast_text = ""
+        self.forecast_text = ""
         for hour in self.hourly_data:
             timestamp = hour["dt"]
             formatted_hour = datetime.utcfromtimestamp(
                 timestamp).strftime('%A, %H:%M')
             main_condition = hour["weather"][0]["main"]
             description = hour["weather"][0]["description"]
-            forcast = (
+            forecast = (
                 f"Time: {formatted_hour}\n\tMain: {main_condition},\n\tDescription: {description}\n")
-            self.forcast_text += forcast
-        return self.forcast_text
+            self.forecast_text += forecast
+        return self.forecast_text
 
 
 # get forcast for week and hours
 weather = Weather()
-hourly_forcast = weather.hourly_forcast()
-weekly_forcast = weather.weekly_forcast()
+hourly_forecast = weather.hourly_forecast()
+weekly_forecast = weather.weekly_forecast()
 
-# text hourly forcast via Twilio
+# get the current date and time 
+today = date.today().strftime("%A")
+time_now = date.today().strftime("%H:%M")
 
+# set up client class for twilio 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-                     body=f"Here is your hourly weather forcast for the day:\n{hourly_forcast}.",
-                     from_='+17738302382',
-                     to=my_phone_number
-                 )
+# text hourly forecast via Twilio at 5am
+if time_now == "5:00":
+    message = client.messages.create(
+                        body=f"Here is your hourly weather forcast for the day:\n{hourly_forecast}.",
+                        from_='+17738302382',
+                        to=my_phone_number
+                    )
 
-# text weekly forcast via Twilio
-
-message = client.messages.create(
-                     body=f"Here is your weekly weather forcast:\n{weekly_forcast}.",
-                     from_='+17738302382',
-                     to=my_phone_number
-                 )
+# text weekly forecast via Twilio every Monday
+if today == "Monday":
+    message = client.messages.create(
+                        body=f"Here is your weekly weather forecast:\n{weekly_forecast}.",
+                        from_='+17738302382',
+                        to=my_phone_number
+                    )
